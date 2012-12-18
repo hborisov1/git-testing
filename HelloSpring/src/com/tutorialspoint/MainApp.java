515eed239be9fca9f0c8d6d4eb1dafc394eb840b
hborisov1 <hborisov1@gmail.com>
2012-12-18 17:30:34 +0200

    some message

--- HelloSpring/src/com/tutorialspoint/MainApp.java
+++ HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -8,8 +8,9 @@ public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-//		int i = 0;
-//		for(;i==15;i++)
+		int i = 0;
+		int j = 0;
+		for(;i==15 && j==10;j++, i++)
 		System.out.println(obj.getMessage());
 	}
 
